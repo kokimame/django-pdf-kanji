cdfed3b7093cca13c3ec021ee994c44af4b28cf8
kokimame <kokimametani@gmail.com>
2019-01-27 21:53:25 +0900

    Test static files

--- joytan_kanji/settings.py
+++ joytan_kanji/settings.py
@@ -29,6 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
+
 
 # Application definition
 
@@ -125,5 +131,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
