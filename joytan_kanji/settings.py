8f847cf088f76603056ab8e1e45b918a0e6875ee
kokimame <kokimametani@gmail.com>
2019-01-27 22:43:36 +0900

    Test static

--- joytan_kanji/settings.py
+++ joytan_kanji/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "sheetgen/static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "sheetgen/static"),
+]
 
 
 
