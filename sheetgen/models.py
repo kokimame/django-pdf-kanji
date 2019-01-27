5f20ea8cbd1fae850c01ce8daf91f3d9790c1544
kokimame <kokimametani@gmail.com>
2019-01-27 22:18:01 +0900

    Added more link

--- sheetgen/models.py
+++ sheetgen/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Generator(models.Model):
-    text_field = models.TextField('Paste some text with kanji. Anything else will be ignored.',)
+    text_field = models.TextField('Paste some text with kanji. Anything else will be ignored.',
+                                  help_text="Currently, we can only process about "\
+                                   "300 characters at once because of free and weak "
+                                   "web hosting.")
